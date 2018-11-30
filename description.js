b99e43ce954da0868766e65f5d0be618af8e4086
ka6rar <karar3931@gmail.com>
2018-11-30 06:14:19 -0800

    .

--- description.js
+++ description.js
@@ -61,38 +61,9 @@ function loadPackageInfo() {
 				$("#screenshot").html(decodeResp.screenshot);
 				$("#screenshot_").show();
 			}
-			if(decodeResp.open == true) {
-				$("#is_open_source_").show();
-			}
-			
         },
 		error: function (err) {
 			$("#errorInfo").html("Description unavailable for "+urlSelfParts[1]);
-		}
-	});
-}
-function loadRecentUpdates() {
-	var form_url = window.location.protocol+"//"+window.location.hostname+"/last.updates";
-	$.ajax({
-		url: form_url,
-		type: "GET",
-		cache: false,
-		crossDomain: true,
-		success: function (returnhtml) {
-			var decodeResp = eval('('+returnhtml+')');
-			var htmlnews = "";
-			for (var dicNow in decodeResp) {
-				var urlOpen = "cydia://package/"+decodeResp[dicNow].package;
-				if (navigator.userAgent.search(/Cydia/) == -1) {
-					urlOpen = window.location.protocol+"//"+window.location.hostname+"/description.html?id="+decodeResp[dicNow].package;
-				}
-				htmlnews +=  "<li><a href='"+urlOpen+"' target='_blank'><img class='icon' src='tweak.png'/><label>"+decodeResp[dicNow].name+" v"+decodeResp[dicNow].version+"</label></a></li>";
-			}
-			$("#updates").html(htmlnews);
-			$("#updates_").show();			
-        },
-		error: function (err) {
-			$("#updates_").hide();	
 		}
 	});
 }
